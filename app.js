cc2447420f0413e450be00e1115f499cae80b8e3
johnpstratton <john.p.stratton6@gmail.com>
2020-04-03 15:51:34 -0700

    this is my multiply branch

--- app.js
+++ app.js
@@ -31,17 +31,27 @@ testSum(4, 7);
 /* Problem 2
 Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
+function testMultiply() { //eslint-disable-line
+  if (multiply(5,9)[0] === 45 && multiply(5,9)[1] === 'The product of 5 and 9 is 45.') {
+    console.log('%c TEST FOR multiply() PASSES', 'color: green');
+  } else {
+    console.log('%c TEST FOR multiply() FAILS', 'color: red');
+  }
+}
+
 "The product of 5 and 9 is 45."
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+var product = a * b
+var stringName = 'The product of ' + a + ' and ' + b + ' is ' + product + '.';
+return [product, stringName];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
