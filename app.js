9997cfb281cfa90ab89cb04a9b7297e8537b460c
Niccoryan0 <niccoryan0@gmail.com>
2020-04-03 16:37:44 -0700

    finished q6

--- app.js
+++ app.js
@@ -118,7 +118,7 @@ function multiplyArray(multArr) { //eslint-disable-line
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-testMultiplyArray(testArray);
+// testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
@@ -142,10 +142,15 @@ Test this function by hand in the console to get it working, and when you think 
 var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+  var total = 1;
+  for(var i = 0; i < dynamicArray.length; i++){
+    total = multiply(total, dynamicArray[i])[0];
+  }
+  var totalString = 'The numbers ' + dynamicArray + ' have a product of ' + total + '.';
+  return [total, totalString];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
