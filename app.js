87082f5821d5f8320bc7cdbd7011ebf6f2595c88
johnpstratton <john.p.stratton6@gmail.com>
2020-04-03 15:31:56 -0700

    This is my test of sum function

--- app.js
+++ app.js
@@ -3,17 +3,27 @@
 /* Problem 1 (this is your demo that we'll solve in class)
 Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
 
-"The sum of 4 and 7 is 11."
+function testSum() { //eslint-disable-line
+  if (sum(4,7)[0] === 11 && sum(4,7)[1] === 'The sum of 4 and 7 is 11.') {
+    console.log('%c TEST FOR sum() PASSES', 'color: green');
+  } else {
+    console.log('%c TEST FOR sum() FAILS', 'color: red');
+  }
+}
+
+[11, "The sum of 4 and 7 is 11."]
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
+  var sumOfAandB = a + b;
+  var rightSentence = 'The sum of ' + a + ' and ' + b + ' is ' + sumOfAandB + '.';
+  return [sumOfAandB, rightSentence]
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
