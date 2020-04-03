e607cf4a6a206d0e33e35c8e8fa369c67b5c921a
Niccoryan0 <niccoryan0@gmail.com>
2020-04-03 16:03:58 -0700

    finished q3

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ function multiply(a, b) { //eslint-disable-line
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+// testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -53,7 +53,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-  
+  var theSum1 = sum(a, b)[0];
+  var theSumTotal = sum(theSum1, c)[0];
+
+  var theProd1 = multiply(a,b)[0];
+  var theProdTotal = multiply(theProd1,c)[0];
+
+  var sumString = a + ' and ' + b + ' and ' + c + ' sum to ' + theSumTotal + '.';
+  var prodString = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + theProdTotal + '.';
+
+  return [theSumTotal, theProdTotal, sumString, prodString];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
