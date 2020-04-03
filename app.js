a7b42ce6e2d43bd1fca298d65801934a8938fee9
d-d-wolfe <dave.wolfe623@gmail.com>
2020-04-03 14:13:19 -0700

    Question 1 complete.

--- app.js
+++ app.js
@@ -9,11 +9,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
+// add and output a string
+//output an array
+// if (sum(4,7)[0] === 11 && sum(4,7)[1] === 'The sum of 4 and 7 is 11.')
+var addedSum = a + b; 
+var concatString = 'the sum of' + a + 'and' + b + 'is' + addedSum
+return [11, 'The sum of 4 and 7 is 11.']
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+ testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
