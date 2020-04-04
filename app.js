19d6c0361251e9fade8a39421e734753129d01cb
Melissa Galush <galushmn@gmail.com>
2020-04-03 18:24:53 -0700

    finished stretch question

--- app.js
+++ app.js
@@ -185,9 +185,16 @@ var testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) {
   //eslint-disable-line
+  let productArray = 1;
+  for (var i = 0; i < dynamicArray.length; i++) {
+    productArray = multiply(dynamicArray[i], productArray)[0];
+  }
+  let multiplyArrayString =
+    'The numbers ' + dynamicArray + ' have a product of ' + productArray + '.';
+  return [productArray, multiplyArrayString];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
