7d22bb026aa3875957c5dda2e291e648be6fc215
MasonChance <mason.fryberger@gmail.com>
2020-04-04 16:28:21 -0700

    multiply function, on multipass branch, with leeluDallas

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ function sum(a, b) { //eslint-disable-line
     var result = 'The sum of ' + a + ' and ' + b + ' is ' + value + '.';
     console.log(result);
     return [value,result];
-    // var resultConcat = array[0];    
+       
 }
 
-// Here is the test for sum(); uncomment it to run it
-testSum();
-sum();
+// // Here is the test for sum(); uncomment it to run it
+// testSum();
+// sum();
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -34,14 +34,21 @@ Write a function called multiply() that takes in two numbers as arguments and re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
-// Write your code here
-// function multiply(a, b) { //eslint-disable-line
+// (multiply(5,9)[0] === 45 && multiply(5,9)[1] === 'The product of 5 and 9 is 45.')
 
-// }
+// Write your code here
+function multiply(a, b) { //eslint-disable-line
+    var product = a * b;
+    console.log(product);
+    var leeluDallas = 'The product of ' + a + ' and ' + b + ' is ' + product + '.';
+    console.log(leeluDallas);
+    return [product, leeluDallas];
+
+}
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
-
+// testMultiply();
+// multiply();
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
