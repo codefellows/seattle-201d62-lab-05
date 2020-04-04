ef3de1d44078c16cc069d80b888170ef8347bcc8
Devin <devingoulet@icloud.com>
2020-04-03 17:41:36 -0700

    question one through 4 still editing

--- app.js
+++ app.js
@@ -9,11 +9,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
+    var theSum = a+b;
+    var sumString = 'the sum of ' + a + ' and ' + b + ' is ' + theSum + '.';
+    console.log(sumString)
+    return [theSum, sumString];
 
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -27,11 +31,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+    var theProduct = a * b;
+    console.log(theProduct);
+    var proString = 'the product of ' + a + ' and ' + b + ' is 45 '
+    console.log(proString);
+    return[theProduct, proString];
 
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -48,11 +57,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+    var sum1 = sum(a, b)[0];
+    var sum2 = sum(sum1, c)[0];
+    var sum3 = a + ' and ' + b + ' and ' + c + 'sum to ' + sum2
 
+    var pro1 = multiply(a, b)[0];
+    var pro2 = multiply(pro1, c)[0];
+    var pro3 = 'the product of ' + a + ' and ' + b + ' and ' + c + ' is ' + pro2
+
+    return[sum2, pro2, sum3, pro3]
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -70,12 +87,22 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+    var arrayTotal = 0;
+
+    for(var i = 0; i < sumArr.length; i++){
+        arrayTotal = sum(arrayTotal, sumArr[i])[0];
+    }
+    console.log(arrayTotal);
+
+    var string2 = sumArr + ' was passed in as an array of numbers, and ' + arrayTotal + ' is their sum';
+    console.log(string2);
+    return [arrayTotal,string2]
 
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
