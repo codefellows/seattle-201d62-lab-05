07c7dc6967cecd579d5bb315e8f7c0847a4d5328
johnpstratton <john.p.stratton6@gmail.com>
2020-04-19 15:08:30 -0700

    MultiplyArray function complete

--- app.js
+++ app.js
@@ -141,11 +141,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
-}
+  var multiplyArrTotal = 1;
+  for (var i = 0; i < multArr.length; i++) {
+    multiplyArrTotal = multiply(multiplyArrTotal, multArr[i])[0];
+  }
+  var multiplyConcatenatedString = 'The numbers ' + multArr[0] + ',' + multArr[1] + ',' + multArr[2] + ' have a product of ' + multiplyArrTotal + '.';
+  return [multiplyArrTotal, multiplyConcatenatedString];
+};
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
